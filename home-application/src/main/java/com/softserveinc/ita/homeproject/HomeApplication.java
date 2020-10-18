91db91b87e0dbcdd0634f13765fc2d97ff691145
Ihor Svyrydenko <djol12345@gmail.com>
2020-10-18 22:12:56 +0300

    Issue#10: Update project skeleton: Dependency management was set instead of parent section in general pom; versions were moved to variables; general groupId was changed; newline at the end of file was added to each pom; packaging of several modules was changed; .keep file was added to each src- and test-directories to keep it in whe commit.

--- home-application/src/main/java/com/softserveinc/ita/homeproject/HomeApplication.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/HomeApplication.java
@@ -1,4 +1,4 @@
-package com.project;
+package com.softserveinc.ita.homeproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
