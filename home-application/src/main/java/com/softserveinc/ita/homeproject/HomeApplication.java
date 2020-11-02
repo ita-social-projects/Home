7c8045a7c5c799460f1140f418344fcacd74c920
mykytam <morar.nikita@gmail.com>
2020-11-02 20:54:26 +0200

    Fix date format in OpenAPI Specification file

--- home-application/src/main/java/com/softserveinc/ita/homeproject/HomeApplication.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/HomeApplication.java
@@ -2,13 +2,11 @@ package com.softserveinc.ita.homeproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication (scanBasePackages = "com.softserveinc.ita.homeproject.config")
-public class HomeApplication extends SpringBootServletInitializer {
+@SpringBootApplication (scanBasePackages = "com.softserveinc.ita.homeproject")
+public class HomeApplication  {
     public static void main(String[] args) {
-         //SpringApplication.run(HomeApplication.class, args);
-         new HomeApplication().configure(new SpringApplicationBuilder(HomeApplication.class)).run(args);
+         SpringApplication.run(HomeApplication.class, args);
+         //new HomeApplication().configure(new SpringApplicationBuilder(HomeApplication.class)).run(args);
     }
 }
--- home-application/src/main/java/com/softserveinc/ita/homeproject/config/JerseyConfig.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/config/JerseyConfig.java
@@ -1,22 +1,13 @@
 package com.softserveinc.ita.homeproject.config;
 
 import com.softserveinc.ita.homeproject.api.NewsApi;
-import com.softserveinc.ita.homeproject.api.NewsApiService;
-import com.softserveinc.ita.homeproject.api.impl.NewsApiServiceImpl;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
-        //register("com.softserveinc.ita.homeproject.api");
         register(NewsApi.class);
-    }
-
-    @Bean
-    public NewsApiService newsApiService(){
-        return new NewsApiServiceImpl();
     }
 }
