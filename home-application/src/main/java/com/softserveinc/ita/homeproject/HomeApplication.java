d0c604c9deb0569501d5fc4632cc6a417e653de1
mykytam <morar.nikita@gmail.com>
2020-11-02 20:23:46 +0200

    Override mustache templates and configure Jersey to Spring bridge

--- home-application/src/main/java/com/softserveinc/ita/homeproject/HomeApplication.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/HomeApplication.java
@@ -2,10 +2,13 @@ package com.softserveinc.ita.homeproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class HomeApplication {
+@SpringBootApplication (scanBasePackages = "com.softserveinc.ita.homeproject.config")
+public class HomeApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-         SpringApplication.run(HomeApplication.class, args);
+         //SpringApplication.run(HomeApplication.class, args);
+         new HomeApplication().configure(new SpringApplicationBuilder(HomeApplication.class)).run(args);
     }
 }
