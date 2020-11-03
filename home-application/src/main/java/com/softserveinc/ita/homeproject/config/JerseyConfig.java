239e5955732ab28ef746da3be949f00bdb374a4d
mykytam <morar.nikita@gmail.com>
2020-11-03 16:04:10 +0200

    Implement package scanning for JerseyConfig

--- home-application/src/main/java/com/softserveinc/ita/homeproject/config/JerseyConfig.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/config/JerseyConfig.java
@@ -1,13 +1,34 @@
 package com.softserveinc.ita.homeproject.config;
 
-import com.softserveinc.ita.homeproject.api.NewsApi;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
 @Component
+@ApplicationPath("/api/0")
 public class JerseyConfig extends ResourceConfig {
 
-    public JerseyConfig() {
-        register(NewsApi.class);
+    public JerseyConfig(ObjectMapper objectMapper) {
+        // register endpoints
+        packages("com.softserveinc.ita.homeproject.api");
+        // register jackson for json
+        register(new ObjectMapperContextResolver(objectMapper));
+    }
+
+    @Provider
+    public static class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
+        private final ObjectMapper mapper;
+        public ObjectMapperContextResolver(ObjectMapper mapper) {
+            this.mapper = mapper;
+        }
+
+        @Override
+        public ObjectMapper getContext(Class<?> type) {
+            return mapper;
+        }
     }
 }
