2b2b43413c037576bc03ffa71db39c0bfa58bd12
mykytam <morar.nikita@gmail.com>
2020-11-05 18:38:23 +0200

    Issue#32: connect generated server to services Delete scanBasePackages Rename module & artifactId to home-server-generator Use JacksonJsonProvider instead of ObjectMapper

--- home-application/src/main/java/com/softserveinc/ita/homeproject/HomeApplication.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/HomeApplication.java
@@ -3,7 +3,7 @@ package com.softserveinc.ita.homeproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication (scanBasePackages = "com.softserveinc.ita.homeproject")
+@SpringBootApplication
 public class HomeApplication  {
     public static void main(String[] args) {
          SpringApplication.run(HomeApplication.class, args);
--- home-application/src/main/java/com/softserveinc/ita/homeproject/config/JerseyConfig.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/config/JerseyConfig.java
@@ -1,34 +1,19 @@
 package com.softserveinc.ita.homeproject.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.ext.ContextResolver;
-import javax.ws.rs.ext.Provider;
 
 @Component
 @ApplicationPath("/api/0")
 public class JerseyConfig extends ResourceConfig {
 
-    public JerseyConfig(ObjectMapper objectMapper) {
+    public JerseyConfig() {
         // register endpoints
         packages("com.softserveinc.ita.homeproject.api");
         // register jackson for json
-        register(new ObjectMapperContextResolver(objectMapper));
-    }
-
-    @Provider
-    public static class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
-        private final ObjectMapper mapper;
-        public ObjectMapperContextResolver(ObjectMapper mapper) {
-            this.mapper = mapper;
-        }
-
-        @Override
-        public ObjectMapper getContext(Class<?> type) {
-            return mapper;
-        }
+        register(JacksonJsonProvider.class);
     }
 }
