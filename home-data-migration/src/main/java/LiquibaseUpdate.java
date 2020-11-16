95be53b08f5892f99ad9e3df24155ffe67696419
Oleksandr Khomenko <okhome@softserveinc.com>
2020-11-17 00:42:54 +0200

    wip

--- home-data-migration/src/main/java/LiquibaseUpdate.java
+++ home-data-migration/src/main/java/LiquibaseUpdate.java
@@ -1,38 +1,32 @@
-import liquibase.Contexts;
-import liquibase.LabelExpression;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import liquibase.Liquibase;
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
-import liquibase.exception.DatabaseException;
 import liquibase.exception.LiquibaseException;
+import liquibase.integration.commandline.CommandLineResourceAccessor;
+import liquibase.resource.CompositeResourceAccessor;
 import liquibase.resource.FileSystemResourceAccessor;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class LiquibaseUpdate {
-   public void runLiquibase(String user, String password) throws DatabaseException, SQLException {
-       java.sql.Connection connection = null;
-       final String url = "jdbc:mysql://localhost:3306/test?createDatabaseIfNotExist=true&serverTimezone=UTC";
-       Liquibase liquibase = null;
-       try {
-           DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver ());
-           connection = DriverManager.getConnection(url, user, password);;
-           Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
-           liquibase = new Liquibase("db.changelog-master.xml", new FileSystemResourceAccessor(), database);
-           liquibase.update("first_migration");
-       } catch (SQLException | LiquibaseException e) {
-           throw new DatabaseException(e);
-       } finally {
-           if (connection != null) {
-               try {
-                   connection.rollback();
-                   connection.close();
-               } catch (SQLException e) {
-                   //nothing to do
-               }
-           }
-       }
-   }
+    public void runLiquibase(String user, String password, String url, String changeLogFile) {
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+
+            FileSystemResourceAccessor fileSystemResourceAccessor = new FileSystemResourceAccessor();
+            ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+            CommandLineResourceAccessor commandLineResourceAccessor = new CommandLineResourceAccessor(classLoader);
+            CompositeResourceAccessor compositeResourceAccessor = new CompositeResourceAccessor(fileSystemResourceAccessor, commandLineResourceAccessor);
+            try (Liquibase liquibase = new Liquibase(changeLogFile, compositeResourceAccessor, database)) {
+                liquibase.update("first_migration");
+            } catch (LiquibaseException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- home-data-migration/src/main/java/Main.java
+++ home-data-migration/src/main/java/Main.java
@@ -1,15 +1,13 @@
-import liquibase.exception.DatabaseException;
-
-import java.sql.SQLException;
-import java.util.Scanner;
+// FIXME Basepackage here
 
 public class Main {
-    public static void main(String[] args) throws DatabaseException, SQLException {
-        String pass, user;
-        Scanner scanner = new Scanner(System.in);
-        user = scanner.nextLine();
-        pass = scanner.nextLine();
+    public static void main(String[] args) {
         LiquibaseUpdate liquibaseUpdate = new LiquibaseUpdate();
-        liquibaseUpdate.runLiquibase(user, pass);
+        liquibaseUpdate.runLiquibase(
+                "root", // FIXME should be taken from args
+                "1234", // FIXME should be taken from args
+                "jdbc:mysql://localhost:3306/test?createDatabaseIfNotExist=true&serverTimezone=UTC", // FIXME should be taken from args
+                "classpath:/db/changelog/db.changelog-master.xml" // FIXME should be taken from args
+        );
     }
 }
