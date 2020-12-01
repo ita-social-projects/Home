8984c2f2420b4b14652927b81f225af912c99789
Ihor Svyrydenko <djol12345@gmail.com>
2020-12-01 23:08:32 +0200

    Issue#56: Implement exception handling Rename Error component to ApiError in open api specification file Delete description field from ApiError Set responseCode field type to integer in ApiError Refactor mapper so it uses generated ApiError

--- home-application/src/main/java/com/softserveinc/ita/homeproject/application/exception/mapper/AlreadyExistExceptionMapper.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/application/exception/mapper/AlreadyExistExceptionMapper.java
@@ -1,6 +1,7 @@
 package com.softserveinc.ita.homeproject.application.exception.mapper;
 
 import com.softserveinc.ita.homeproject.homeservice.exception.AlreadyExistException;
+import com.softserveinc.ita.homeproject.model.ApiError;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -10,6 +11,10 @@ import javax.ws.rs.ext.Provider;
 public class AlreadyExistExceptionMapper implements ExceptionMapper<AlreadyExistException> {
     @Override
     public Response toResponse(AlreadyExistException exception) {
-        return Response.status(Response.Status.CONFLICT).entity(exception.getMessage()).build();
+        return Response.status(Response.Status.CONFLICT)
+                .entity(new ApiError()
+                        .responseCode(Response.Status.CONFLICT.getStatusCode())
+                        .errorMessage(exception.getMessage()))
+                .build();
     }
 }
--- home-application/src/main/java/com/softserveinc/ita/homeproject/application/exception/mapper/NotFoundExceptionMapper.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/application/exception/mapper/NotFoundExceptionMapper.java
@@ -1,6 +1,7 @@
 package com.softserveinc.ita.homeproject.application.exception.mapper;
 
 import com.softserveinc.ita.homeproject.homeservice.exception.NotFoundException;
+import com.softserveinc.ita.homeproject.model.ApiError;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -10,6 +11,10 @@ import javax.ws.rs.ext.Provider;
 public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
     @Override
     public Response toResponse(NotFoundException exception) {
-        return Response.status(Response.Status.NOT_FOUND).entity(exception.getMessage()).build();
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity(new ApiError()
+                        .responseCode(Response.Status.NOT_FOUND.getStatusCode())
+                        .errorMessage(exception.getMessage()))
+                .build();
     }
 }
