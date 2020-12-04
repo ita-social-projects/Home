adf3700ee5ad9b7889c13902ded51a604b088349
mykytam <morar.nikita@gmail.com>
2020-12-04 13:01:02 +0200

    Issue#79: Document project files Add description to repositories

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/repository/NewsRepository.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/repository/NewsRepository.java
@@ -4,6 +4,12 @@ import com.softserveinc.ita.homeproject.homedata.entity.News;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * NewsRepository is the interface that is needed
+ * for interaction with News in the database.
+ *
+ * @author Mykyta Morar
+ */
 @Repository
 public interface NewsRepository extends PagingAndSortingRepository<News, Long> {
 }
