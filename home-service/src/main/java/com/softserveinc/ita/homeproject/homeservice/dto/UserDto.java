69288584e1fef1829dd4c3b0a80ef8c0fc882b7f
alexorlenko <https://github.com/alexorlenko/springSecuryti.git>
2020-12-07 11:29:31 +0200

    Add change into a maven plugin for correct work Lombok and mapstruct.

--- home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/dto/UserDto.java
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/dto/UserDto.java
@@ -1,8 +1,12 @@
 package com.softserveinc.ita.homeproject.homeservice.dto;
 
-import lombok.Builder;
+import lombok.*;
 
+@Getter
+@Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto extends BaseDto {
 
     private Long id;
@@ -16,64 +20,4 @@ public class UserDto extends BaseDto {
     private String password;
 
     private String contacts;
-
-    public UserDto() {
-    }
-
-    public UserDto(Long id, String firstName, String lastName, String email, String password, String contacts) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.contacts = contacts;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getContacts() {
-        return contacts;
-    }
-
-    public void setContacts(String contacts) {
-        this.contacts = contacts;
-    }
 }
