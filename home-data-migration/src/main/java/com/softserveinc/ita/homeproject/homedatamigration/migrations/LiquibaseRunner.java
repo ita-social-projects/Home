337124f5cc7848259f9cd2adb277da313e344be3
Valera3107 <valerapetruna3107@gmail.com>
2020-12-16 15:56:30 +0200

    Add property for heroku Add opportunity to connect with full url, that includes user and password Delete class Builder from LiquibaseUpdate

--- home-data-migration/src/main/java/com/softserveinc/ita/homeproject/homedatamigration/migrations/LiquibaseRunner.java
+++ home-data-migration/src/main/java/com/softserveinc/ita/homeproject/homedatamigration/migrations/LiquibaseRunner.java
@@ -2,30 +2,48 @@ package com.softserveinc.ita.homeproject.homedatamigration.migrations;
 
 import liquibase.exception.LiquibaseException;
 
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class LiquibaseRunner {
     public static void main(String[] args) throws LiquibaseException, SQLException {
-        //default value
-        String url = "jdbc:postgresql://localhost:5432/test";
-        String user = "postgres";
-        String pass = "1234";
+        // Full url to connecting to Db on Heroku
+        // String urljdbc = "jdbc:postgresql://ec2-34-254-24-116.eu-west-1.compute.amazonaws.com:5432/d1pi5jhrf0fbun?user=hwkbjsunlfuchw&password=c9039cc29d3150b2288c58d1eb988c7ea9a3a5b38eb117f39b5cb9bbf6fccd76";
+        String user = LiquibaseUpdate.getFromProperties(LiquibaseUpdate.PROPERTYFILENAME,
+                "user");
+        String pass = LiquibaseUpdate.getFromProperties(LiquibaseUpdate.PROPERTYFILENAME,
+                "password");
+        String url = LiquibaseUpdate.getFromProperties(LiquibaseUpdate.PROPERTYFILENAME,
+                "url");
+        boolean checkFullUrl = false;
+        // read args from command line
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("-p")) {
                 i++;
                 pass = args[i];
-            } else if (args[i].equals("-us")) {
+            } else if (args[i].equals("-user")) {
                 i++;
                 user = args[i];
-            } else if (args[i].equals("-u")) {
+            } else if (args[i].equals("-url")) {
                 i++;
                 url = args[i];
+                // full url with password and user
+            } else if (args[i].equals("-furl")) {
+                i++;
+                url = args[i];
+                checkFullUrl = true;
             }
         }
-        LiquibaseUpdate liquibaseUpdate = LiquibaseUpdate.newBuilder()
-                .setUrl(url)
-                .setUser(user)
-                .setPassword(pass).build();
+        LiquibaseUpdate liquibaseUpdate = setParam(url, user, pass, checkFullUrl);
         liquibaseUpdate.runLiquibase();
+    }
+    private static LiquibaseUpdate setParam(String url, String user, String password, boolean check) throws SQLException {
+        LiquibaseUpdate liquibaseUpdate;
+        if (check) {
+            liquibaseUpdate = new LiquibaseUpdate(DriverManager.getConnection(url));
+        } else {
+            liquibaseUpdate = new LiquibaseUpdate(DriverManager.getConnection(url, user, password));
+        }
+        return liquibaseUpdate;
     }
 }
--- home-data-migration/src/main/java/com/softserveinc/ita/homeproject/homedatamigration/migrations/LiquibaseUpdate.java
+++ home-data-migration/src/main/java/com/softserveinc/ita/homeproject/homedatamigration/migrations/LiquibaseUpdate.java
@@ -14,47 +14,18 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class LiquibaseUpdate {
-    private String url;
-    private String user;
-    private String password;
+    private Connection connection_for_db;
     //    name of references in migration.properties
-    private static final String PATHNAME = "changelog.path";
-    private final static String PROPERTYFILENAME = "migration";
+    static final String PATHNAME = "changelog.path";
+    final static String PROPERTYFILENAME = "migration";
 
-    private LiquibaseUpdate() {
-    }
-
-    public static Builder newBuilder() {
-        return new LiquibaseUpdate().new Builder();
-    }
-
-    public class Builder {
-        private Builder() {
-        }
-
-        public Builder setUrl(String url) {
-            LiquibaseUpdate.this.url = url;
-            return this;
-        }
-
-        public Builder setUser(String user) {
-            LiquibaseUpdate.this.user = user;
-            return this;
-        }
-
-        public Builder setPassword(String password) {
-            LiquibaseUpdate.this.password = password;
-            return this;
-        }
-
-        public LiquibaseUpdate build() {
-            return LiquibaseUpdate.this;
-        }
+    public LiquibaseUpdate(Connection connection_for_db) {
+        this.connection_for_db = connection_for_db;
     }
 
     public void runLiquibase() throws LiquibaseException, SQLException {
         String path = getFromProperties(PROPERTYFILENAME, PATHNAME);
-        Connection connection = DriverManager.getConnection(url, user, password);
+        Connection connection = connection_for_db;
         DriverManager.registerDriver(new Driver());
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         CommandLineResourceAccessor commandLineResourceAccessor = new CommandLineResourceAccessor(classLoader);
@@ -62,8 +33,9 @@ public class LiquibaseUpdate {
         Liquibase liquibase = new Liquibase(path, compositeResourceAccessor, new JdbcConnection(connection));
         liquibase.update(new Contexts());
     }
+
     //    Get value from migration.properties
-    public String getFromProperties(String propertyFileName, String propertyKey) {
+    public static String getFromProperties(String propertyFileName, String propertyKey) {
         ResourceBundle rb = ResourceBundle.getBundle(propertyFileName);
         return rb.keySet().stream().filter(key -> key.equals(propertyKey))
                 .map(key -> rb.getString(key))
