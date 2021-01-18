e5a6e656bd3b1e7775072ac711e1d23e5e863c49
Dandria1802 <69143066+Dandria1802@users.noreply.github.com>
2021-01-18 11:11:50 +0200

    Issue#97 HouseCRUD. Deleted Address

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.softserveinc.ita.homeproject.homedata.repository;
-
-import com.softserveinc.ita.homeproject.homedata.entity.Addresses;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface AddressRepository extends PagingAndSortingRepository<Addresses, Long> {
-}
