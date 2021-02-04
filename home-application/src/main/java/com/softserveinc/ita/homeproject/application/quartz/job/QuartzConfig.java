1dff365f01f952ebe85bd3e2582748a10b6ed43c
Dandria1802 <69143066+Dandria1802@users.noreply.github.com>
2021-02-04 11:05:49 +0200

    Issue#127 ConfigureQuartz. Added a basic working configuration. Added tables for store QuartzData.

---
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/application/quartz/job/QuartzConfig.java
@@ -0,0 +1,34 @@
+package com.softserveinc.ita.homeproject.application.quartz.job;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobDetail;
+import org.quartz.SimpleTrigger;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
+
+@Slf4j
+@Configuration
+public class QuartzConfig {
+
+    static JobDetailFactoryBean createJobDetail(Class jobClass, String jobName) {
+        log.debug("createJobDetail(jobClass={}, jobName={})", jobClass.getName(), jobName);
+        JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
+        factoryBean.setName(jobName);
+        factoryBean.setJobClass(jobClass);
+        factoryBean.setDurability(true);
+        return factoryBean;
+    }
+    static SimpleTriggerFactoryBean createTrigger(JobDetail jobDetail, long pollFrequencyMs, String triggerName) {
+        log.debug("createTrigger(jobDetail={}, pollFrequencyMs={}, triggerName={})", jobDetail.toString(), pollFrequencyMs, triggerName);
+        SimpleTriggerFactoryBean factoryBean = new SimpleTriggerFactoryBean();
+        factoryBean.setJobDetail(jobDetail);
+        factoryBean.setStartDelay(0L);
+        factoryBean.setRepeatInterval(pollFrequencyMs);
+        factoryBean.setName(triggerName);
+        factoryBean.setRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY);
+        factoryBean.setMisfireInstruction(SimpleTrigger.MISFIRE_INSTRUCTION_RESCHEDULE_NEXT_WITH_REMAINING_COUNT);
+        return factoryBean;
+    }
+}
