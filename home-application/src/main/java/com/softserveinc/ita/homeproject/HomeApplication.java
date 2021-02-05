6f3907dc753b90e219762a80b77e79b5f20cc918
Polina <polinabut97@gmail.com>
2021-02-05 13:05:14 +0200

    Issue#127: configured application.properties files

--- home-application/src/main/java/com/softserveinc/ita/homeproject/HomeApplication.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/HomeApplication.java
@@ -2,10 +2,15 @@ package com.softserveinc.ita.homeproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-public class HomeApplication  {
+@PropertySource({
+        "data-application.properties",
+        "service-application.properties"
+})
+public class HomeApplication {
     public static void main(String[] args) {
-         SpringApplication.run(HomeApplication.class, args);
+        SpringApplication.run(HomeApplication.class, args);
     }
 }
