e3820a6861d9931cd3d8614e254b61f54bfb53ad
OlTym <53350727+OlTym@users.noreply.github.com>
2021-02-14 20:15:42 +0200

    Update NewsRepository.java

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/repository/NewsRepository.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/repository/NewsRepository.java
@@ -4,6 +4,7 @@ import com.softserveinc.ita.homeproject.homedata.entity.News;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+
 /**
  * UserRepository is the interface that is needed
  * for interaction with Users in the database.
