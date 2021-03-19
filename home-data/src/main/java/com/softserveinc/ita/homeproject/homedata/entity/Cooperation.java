12f781d4696c288c6df2126bdce3df53de7955b2
Vadym Sokorenko <69482838+ElgorTheWanderer@users.noreply.github.com>
2021-03-19 17:51:56 +0200

    Issue#134: Added Cooperation sequence.

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/entity/Cooperation.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/entity/Cooperation.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +13,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "cooperation")
+@SequenceGenerator(name = "sequence", sequenceName = "cooperation_sequence")
 public class Cooperation extends BaseEntity implements Serializable {
 
     @OneToMany(mappedBy = "cooperation")
