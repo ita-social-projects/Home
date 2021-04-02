7c4d843cb74ea8e09e2af7686a8c0a4f9c8d38d2
Oleksii Zinkevych <zinkevich-as@ukr.net>
2021-04-02 13:35:55 +0300

    Issue#193: Moved common local variables to constants in ApiClientUtil Synced changes with Issue#194 branch in ApiClientUtil

--- home-api-tests/src/test/java/com/softserveinc/ita/homeproject/api/tests/utils/ApiClientUtil.java
+++ home-api-tests/src/test/java/com/softserveinc/ita/homeproject/api/tests/utils/ApiClientUtil.java
@@ -10,31 +10,33 @@ import com.softserveinc.ita.homeproject.ServerConfiguration;
 import org.glassfish.jersey.logging.LoggingFeature;
 
 public final class ApiClientUtil {
+    private static final String APPLICATION_EXTERNAL_PORT = System.getProperty("home.application.external.port");
+    private static final String APPLICATION_ADMIN_USER_NAME = System.getProperty("home.application.admin.username");
+    private static final String APPLICATION_ADMIN_PASSWORD = System.getProperty("home.application.admin.password");
+    private static final String VERBOSE_LOGGING = System.getProperty("verbose.tests.logging", "true");
+    private static final ApiClient CLIENT = new ApiClient();
 
     public static ApiClient getClient() {
-        String applicationExternalPort = System.getProperty("home.application.external.port");
-        String applicationAdminUsername = System.getProperty("home.application.admin.username");
-        String applicationAdminPassword = System.getProperty("home.application.admin.password");
-        ApiClient client = new ApiClient();
-        Logger logger = Logger.getLogger(ApiClient.class.getName());
-        client.getHttpClient()
-            .register(new LoggingFeature(logger, Level.INFO, LoggingFeature.Verbosity.PAYLOAD_ANY, 8192));
-        client.setUsername(applicationAdminUsername);
-        client.setPassword(applicationAdminPassword);
-        client.setServers(List.of(new ServerConfiguration("http://localhost:" + applicationExternalPort + "/api/0",
-            "No description provided", new HashMap())));
-        return client;
+        setLoggingFeature();
+        CLIENT.setUsername(APPLICATION_ADMIN_USER_NAME);
+        CLIENT.setPassword(APPLICATION_ADMIN_PASSWORD);
+        CLIENT.setServers(List.of(new ServerConfiguration("http://localhost:" +
+                APPLICATION_EXTERNAL_PORT + "/api/0", "No description provided", new HashMap())));
+        return CLIENT;
     }
 
     public static ApiClient getUnauthorizedClient() {
-        String applicationExternalPort = System.getProperty("home.application.external.port");
-        ApiClient client = new ApiClient();
-        Logger logger = Logger.getLogger(ApiClient.class.getName());
-        client.getHttpClient()
-            .register(new LoggingFeature(logger, Level.INFO, LoggingFeature.Verbosity.PAYLOAD_ANY, 8192));
-        client.setServers(List.of(new ServerConfiguration("http://localhost:" + applicationExternalPort + "/api/0",
-                "No description provided", new HashMap())));
-        return client;
+        setLoggingFeature();
+        CLIENT.setServers(List.of(new ServerConfiguration("http://localhost:" +
+                APPLICATION_EXTERNAL_PORT + "/api/0", "No description provided", new HashMap())));
+        return CLIENT;
+    }
+
+    private static void setLoggingFeature() {
+        if (Boolean.parseBoolean(VERBOSE_LOGGING)) {
+            Logger logger = Logger.getLogger(ApiClient.class.getName());
+            CLIENT.getHttpClient()
+               .register(new LoggingFeature(logger, Level.INFO, LoggingFeature.Verbosity.PAYLOAD_ANY, 8192));
+        }
     }
 }
-
