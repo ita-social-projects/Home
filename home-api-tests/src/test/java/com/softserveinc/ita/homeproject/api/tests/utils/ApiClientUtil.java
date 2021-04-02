c21bb9ed8642b58cb29f531740825a1afccfa288
Yura Horb <g.y.andreevich@gmail.com>
2021-04-02 13:35:56 +0300

    Issue#194 added one command on home-api-tests.md; synced changes with Issue#193 branch in ApiClientUtil

--- home-api-tests/src/test/java/com/softserveinc/ita/homeproject/api/tests/utils/ApiClientUtil.java
+++ home-api-tests/src/test/java/com/softserveinc/ita/homeproject/api/tests/utils/ApiClientUtil.java
@@ -10,25 +10,33 @@ import com.softserveinc.ita.homeproject.ServerConfiguration;
 import org.glassfish.jersey.logging.LoggingFeature;
 
 public final class ApiClientUtil {
+    private static final String APPLICATION_EXTERNAL_PORT = System.getProperty("home.application.external.port");
+    private static final String APPLICATION_ADMIN_USER_NAME = System.getProperty("home.application.admin.username");
+    private static final String APPLICATION_ADMIN_PASSWORD = System.getProperty("home.application.admin.password");
+    private static final String VERBOSE_LOGGING = System.getProperty("verbose.tests.logging", "true");
+    private static final ApiClient CLIENT = new ApiClient();
 
     public static ApiClient getClient() {
-        String applicationExternalPort = System.getProperty("home.application.external.port");
-        String applicationAdminUsername = System.getProperty("home.application.admin.username");
-        String applicationAdminPassword = System.getProperty("home.application.admin.password");
-        String verboseLogging = System.getProperty("verbose.tests.logging", "true");
-        ApiClient client = new ApiClient();
-        Logger logger = Logger.getLogger(ApiClient.class.getName());
-        Level levelOfLogging = Level.WARNING;
-        if (Boolean.parseBoolean(verboseLogging)) {
-            levelOfLogging = Level.INFO;
+        setLoggingFeature();
+        CLIENT.setUsername(APPLICATION_ADMIN_USER_NAME);
+        CLIENT.setPassword(APPLICATION_ADMIN_PASSWORD);
+        CLIENT.setServers(List.of(new ServerConfiguration("http://localhost:" +
+            APPLICATION_EXTERNAL_PORT + "/api/0", "No description provided", new HashMap())));
+        return CLIENT;
+    }
+
+    public static ApiClient getUnauthorizedClient() {
+        setLoggingFeature();
+        CLIENT.setServers(List.of(new ServerConfiguration("http://localhost:" +
+            APPLICATION_EXTERNAL_PORT + "/api/0", "No description provided", new HashMap())));
+        return CLIENT;
+    }
+
+    private static void setLoggingFeature() {
+        if (Boolean.parseBoolean(VERBOSE_LOGGING)) {
+            Logger logger = Logger.getLogger(ApiClient.class.getName());
+            CLIENT.getHttpClient()
+               .register(new LoggingFeature(logger, Level.INFO, LoggingFeature.Verbosity.PAYLOAD_ANY, 8192));
         }
-        client.getHttpClient()
-            .register(new LoggingFeature(logger, levelOfLogging, LoggingFeature.Verbosity.PAYLOAD_ANY, 8192));
-        client.setUsername(applicationAdminUsername);
-        client.setPassword(applicationAdminPassword);
-        client.setServers(List.of(new ServerConfiguration("http://localhost:" + applicationExternalPort + "/api/0",
-            "No description provided", new HashMap())));
-        return client;
     }
 }
-
