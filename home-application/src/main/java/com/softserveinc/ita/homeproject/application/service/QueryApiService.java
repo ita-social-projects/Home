4d62a809a129ebd4d3e06c35c87cc08c9f0868cc
Oleksii Zinkevych <zinkevich-as@ukr.net>
2021-04-22 17:18:09 +0300

    Issue#202: Changes in QueryApiService - changed getParameterValue method to return values of path parameters also - changed getPageFromQuery method to throw NotFoundHomeException when returned Page is empty - added getOne method to add ability to use QueryApiService for getting one element by id - changed javadoc

--- home-application/src/main/java/com/softserveinc/ita/homeproject/application/service/QueryApiService.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/application/service/QueryApiService.java
@@ -15,13 +15,13 @@ import javax.ws.rs.core.UriInfo;
 import com.softserveinc.ita.homeproject.homedata.entity.BaseEntity;
 import com.softserveinc.ita.homeproject.homeservice.dto.BaseDto;
 import com.softserveinc.ita.homeproject.homeservice.exception.BadRequestHomeException;
+import com.softserveinc.ita.homeproject.homeservice.exception.NotFoundHomeException;
 import com.softserveinc.ita.homeproject.homeservice.service.QueryableService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
 
 /**
  * QueryApiService - service that provides Spring Data Page by request query
- *
  * @author Oleksii Zinkevych
  * @param <T> - entity type
  * @param <D> - DTO type
@@ -32,11 +32,12 @@ public interface QueryApiService<T extends BaseEntity, D extends BaseDto> {
             .collect(Collectors.toMap(DefaultQueryParams::getParameter, DefaultQueryParams::getValue));
 
     /**
-     *
      * @param uriInfo - object that implements UriInfo interface
      *                  that provides access to application and request URI information
      * @return Map of query and path parameters required by EntitySpecificationService
      * to build Filter specification
+     * @throws BadRequestHomeException if uriInfo contains path and query params with the same name
+     * @throws BadRequestHomeException if uriInfo contains query params with more than one value
      */
     static MultivaluedMap<String, String> getFilterMap(UriInfo uriInfo) {
         MultivaluedMap<String, String> queryParams = uriInfo.getQueryParameters();
@@ -61,7 +62,6 @@ public interface QueryApiService<T extends BaseEntity, D extends BaseDto> {
     }
 
     /**
-     *
      * @param uriInfo - object that implements UriInfo interface
      *                  that provides access to application and request URI information
      * @return Spring Data Specification of entity
@@ -69,8 +69,7 @@ public interface QueryApiService<T extends BaseEntity, D extends BaseDto> {
     Specification<T> getSpecification(UriInfo uriInfo);
 
     /**
-     *
-     * @param param   - String name of query parameter
+     * @param param   - String name of query or path parameter
      * @param uriInfo - object that implements UriInfo interface
      *                  that provides access to application and request URI information
      * @return Optional of String value of specified query parameter
@@ -80,24 +79,54 @@ public interface QueryApiService<T extends BaseEntity, D extends BaseDto> {
             return Optional.of(uriInfo.getQueryParameters()
                     .get(param)
                     .get(0));
+        } else if (uriInfo.getPathParameters().get(param) != null) {
+            return Optional.of(uriInfo.getPathParameters()
+                    .get(param)
+                    .get(0));
         } else {
             return Optional.empty();
         }
     }
 
     /**
-     *
      * @param uriInfo - object that implements UriInfo interface
      *                  that provides access to application and request URI information
      * @param service - implementation of QueryableService interface
      * @return Spring Data Page of DTOs according to request query
+     * @throws NotFoundHomeException if number of Page elements less than 1
      */
     default Page<D> getPageFromQuery(UriInfo uriInfo, QueryableService<T, D> service) {
         int pageNumber = Integer.parseInt(getParameterValue(DefaultQueryParams.PAGE_NUMBER.getParameter(), uriInfo)
                 .orElse(DefaultQueryParams.PAGE_NUMBER.getValue()));
         int pageSize = Integer.parseInt(getParameterValue(DefaultQueryParams.PAGE_SIZE.getParameter(), uriInfo)
                 .orElse(DefaultQueryParams.PAGE_SIZE.getValue()));
-        return service.findAll(pageNumber, pageSize, getSpecification(uriInfo));
+        Page<D> page = service.findAll(pageNumber, pageSize, getSpecification(uriInfo));
+        if (page.getTotalElements() > 0) {
+            return page;
+        } else {
+            throw new NotFoundHomeException("No entities found by defined query");
+        }
+    }
+
+    /**
+     * @param uriInfo - object that implements UriInfo interface
+     *                  that provides access to application and request URI information
+     * @param service - implementation of QueryableService interface
+     * @return Single DTO
+     * @throws NotFoundHomeException if number of Page elements less than 1
+     * @throws IllegalStateException if number of Page elements more than 1
+     */
+    default D getOne(UriInfo uriInfo, QueryableService<T, D> service) {
+        Page<D> page = getPageFromQuery(uriInfo, service);
+        if (page.getTotalElements() == 1) {
+            return page.getContent().get(0);
+        } else if (page.getTotalElements() == 0) {
+            throw new NotFoundHomeException("Entity with id: " + getParameterValue("id", uriInfo)
+                    .orElse("'not defined'") + " is not found");
+        } else {
+            throw new IllegalStateException("Result of the request that required to return one element, "
+                    + "contains more than one element");
+        }
     }
 
     /**
