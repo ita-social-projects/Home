42103af39b1ce378d90df82b94d071a42a34424c
Rossokha Ilya <ayliross21@gmail.com>
2021-04-30 15:52:17 +0300

    Issue#180 api apartment implementation

---
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/application/api/HouseApiImpl.java
@@ -0,0 +1,75 @@
+package com.softserveinc.ita.homeproject.application.api;
+
+import com.softserveinc.ita.homeproject.api.HousesApi;
+import com.softserveinc.ita.homeproject.homeservice.dto.ApartmentDto;
+import com.softserveinc.ita.homeproject.homeservice.service.ApartmentService;
+import com.softserveinc.ita.homeproject.model.CreateApartment;
+import com.softserveinc.ita.homeproject.model.ReadApartment;
+import com.softserveinc.ita.homeproject.model.UpdateApartment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+import java.math.BigDecimal;
+
+import static com.softserveinc.ita.homeproject.application.constants.Permissions.*;
+
+@Provider
+@Component
+public class HouseApiImpl extends CommonApi implements HousesApi {
+
+    @Autowired
+    private ApartmentService apartmentService;
+
+    @PreAuthorize(CREATE_APARTMENT_PERMISSION)
+    @Override
+    public Response createApartment(Long houseId, @Valid CreateApartment createApartment) {
+        var createApartmentDto = mapper.convert(createApartment, ApartmentDto.class);
+        var readApartmentDto = apartmentService.createApartment(houseId, createApartmentDto);
+        var readApartment = mapper.convert(readApartmentDto, ReadApartment.class);
+
+        return Response.status(Response.Status.CREATED).entity(readApartment).build();
+    }
+
+    @Override
+    public Response deleteApartment(Long houseId, Long id) {
+        return null;
+    }
+
+    @PreAuthorize(GET_APARTMENT_PERMISSION)
+    @Override
+    public Response getApartment(Long houseId, Long id) {
+        ApartmentDto toGet = apartmentService.getApartmentById(houseId, id);
+        var readApartment = mapper.convert(toGet, ReadApartment.class);
+
+        return Response.status(Response.Status.OK).entity(readApartment).build();
+    }
+
+    @PreAuthorize(GET_APARTMENTS_PERMISSION)
+    @Override
+    public Response queryApartment(@Min(1L) Long houseId,
+                                   @Min(1) Integer pageNumber,
+                                   @Min(1) @Max(10) Integer pageSize,
+                                   String sort,
+                                   String filter,
+                                   Long id,
+                                   @Pattern(regexp = "^(?!(0))\\d{1,4}(\\-[a-zа-я])?$") @Size(min = 1, max = 6) String apartmentNumber,
+                                   BigDecimal apartmentArea) {
+        Page<ApartmentDto> readApartment = queryApiService.getPageFromQuery(uriInfo, apartmentService);
+        return buildQueryResponse(readApartment, ReadApartment.class);
+    }
+
+
+    @Override
+    public Response updateApartment(Long houseId, Long id, @Valid UpdateApartment updateApartment) {
+        return null;
+    }
+}
