dc83343976f516908c7daa64b0eaec20e29506e0
Rossokha Ilya <ayliross21@gmail.com>
2021-04-30 15:51:24 +0300

    Issue#180 RSQL query config for apartments

---
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/application/config/query/rsql/ApartmentRSQLEndpointConfig.java
@@ -0,0 +1,33 @@
+package com.softserveinc.ita.homeproject.application.config.query.rsql;
+
+import com.softserveinc.ita.homeproject.application.config.query.ApartmentQueryConfig;
+import com.softserveinc.ita.homeproject.application.config.query.QueryConfig;
+import com.softserveinc.ita.homeproject.homedata.entity.Apartment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+@Component
+public class ApartmentRSQLEndpointConfig implements RSQLEndpointConfig <Apartment, ApartmentQueryConfig.ApartmentQueryParamEnum> {
+
+    @Autowired
+    private ApartmentQueryConfig queryConfig;
+
+    @Override
+    public Map<ApartmentQueryConfig.ApartmentQueryParamEnum, String> getMappings() {
+        EnumMap<ApartmentQueryConfig.ApartmentQueryParamEnum, String> map = new EnumMap<>(ApartmentQueryConfig.ApartmentQueryParamEnum.class);
+
+        map.put(ApartmentQueryConfig.ApartmentQueryParamEnum.HOUSE_ID, "house_id");
+        map.put(ApartmentQueryConfig.ApartmentQueryParamEnum.APARTMENT_NUMBER, "apartment_number");
+        map.put(ApartmentQueryConfig.ApartmentQueryParamEnum.APARTMENT_AREA, "apartment_area");
+
+        return map;
+    }
+
+    @Override
+    public QueryConfig<Apartment> getQueryConfig() {
+        return queryConfig;
+    }
+}
