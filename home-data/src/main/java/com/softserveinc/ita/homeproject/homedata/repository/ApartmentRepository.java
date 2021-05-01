b03f917fd96297f038e4c0b492ccc2427b05c9b1
Rossokha Ilya <ayliross21@gmail.com>
2021-05-01 15:46:54 +0300

    Issue#180 code smell fix

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/repository/ApartmentRepository.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/repository/ApartmentRepository.java
@@ -1,7 +1,6 @@
 package com.softserveinc.ita.homeproject.homedata.repository;
 
 import com.softserveinc.ita.homeproject.homedata.entity.Apartment;
-import com.softserveinc.ita.homeproject.homedata.entity.Contact;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
