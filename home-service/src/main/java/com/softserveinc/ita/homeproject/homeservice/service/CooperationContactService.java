327bcc50f3012020d3d05b196aeab551ecc68fad
Oleksii Zinkevych <zinkevich-as@ukr.net>
2021-05-12 16:29:48 +0300

    Issue#218: Changes in CooperationContactService - deleted getContactById method as it duplicated with getOne in QueryableService

--- home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/service/CooperationContactService.java
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/service/CooperationContactService.java
@@ -3,6 +3,4 @@ package com.softserveinc.ita.homeproject.homeservice.service;
 import com.softserveinc.ita.homeproject.homeservice.dto.ContactDto;
 
 public interface CooperationContactService extends ContactService {
-
-    ContactDto getContactById(Long id);
 }
