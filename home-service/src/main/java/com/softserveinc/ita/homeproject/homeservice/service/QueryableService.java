9d6b1a8d428da826f59e1e1b252066c00b37444d
Oleksii Zinkevych <zinkevich-as@ukr.net>
2021-05-12 16:24:57 +0300

    Issue#218: Changes in QueryableService - added getOne method and support methods getMessage and getEntityClassName

--- home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/service/QueryableService.java
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/service/QueryableService.java
@@ -1,15 +1,19 @@
 package com.softserveinc.ita.homeproject.homeservice.service;
 
+import java.util.Optional;
+
 import com.softserveinc.ita.homeproject.homedata.entity.BaseEntity;
 import com.softserveinc.ita.homeproject.homeservice.dto.BaseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * QueryableService - service that provides Spring Data Page by entity specification
  * and page parameters
- *
  * @author Oleksii Zinkevych
+ * @param <T> - entity type
+ * @param <D> - DTO type
  */
 public interface QueryableService<T extends BaseEntity, D extends BaseDto> {
     Page<D> findAll(Integer pageNumber, Integer pageSize, Specification<T> specification);
