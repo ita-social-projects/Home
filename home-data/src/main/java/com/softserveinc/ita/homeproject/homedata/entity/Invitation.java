0be25c9f140da7eb18ef5056c3a221749f6b8f72
Rossokha Ilya <ayliross21@gmail.com>
2021-05-13 17:56:18 +0300

    Issue#180 invitation model changed

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/entity/Invitation.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/entity/Invitation.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.SequenceGenerator;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,14 +11,10 @@ import lombok.Setter;
 @Getter
 @Setter
 @MappedSuperclass
-@SequenceGenerator(name = "sequence", sequenceName = "invitations_sequence")
 public abstract class Invitation extends BaseEntity {
 
     @Column(name = "email")
     private String email;
-
-    @Column(name = "name")
-    private String name;
 
     @Convert(converter = InvitationStatusAttributeConverter.class)
     @Column(name = "status")
