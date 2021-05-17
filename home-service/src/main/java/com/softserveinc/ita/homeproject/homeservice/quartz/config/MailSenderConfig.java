d1f281c869f215ef86d2f54a84c4a45053b9c79a
BenAvleck <ben.avleck@gmail.com>
2021-05-17 11:46:52 +0300

    Issue#127: Add configuration file for Mail sender config.

---
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/quartz/config/MailSenderConfig.java
@@ -0,0 +1,29 @@
+package com.softserveinc.ita.homeproject.homeservice.quartz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailSenderConfig {
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("my.gmail@gmail.com");
+        mailSender.setPassword("password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
