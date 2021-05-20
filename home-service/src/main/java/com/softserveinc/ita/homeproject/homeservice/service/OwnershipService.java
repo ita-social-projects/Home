c20d36ccd52a460ef6d52473d6f4c1c66d2a5f6a
Rossokha Ilya <ayliross21@gmail.com>
2021-05-20 17:04:24 +0300

    Issue#183 ownerships service, rsql config, liquibase test data

---
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/service/OwnershipService.java
@@ -0,0 +1,13 @@
+package com.softserveinc.ita.homeproject.homeservice.service;
+
+import com.softserveinc.ita.homeproject.homedata.entity.Ownership;
+import com.softserveinc.ita.homeproject.homeservice.dto.OwnershipDto;
+
+public interface OwnershipService extends QueryableService <Ownership, OwnershipDto> {
+
+    OwnershipDto getOwnershipById(Long apartmentId, Long id);
+
+    OwnershipDto updateOwnership(Long apartmentId, Long id, OwnershipDto updateOwnershipDto);
+
+    void deactivateOwnershipById(Long apartmentId, Long id);
+}
