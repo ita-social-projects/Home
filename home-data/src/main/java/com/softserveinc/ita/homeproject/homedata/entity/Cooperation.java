8d1fd956990f6845972174936a730c7c41f54ed9
Yura Horb <g.y.andreevich@gmail.com>
2021-06-02 15:21:34 +0300

    Issue#127 change condition

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/entity/Cooperation.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/entity/Cooperation.java
@@ -7,6 +7,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
