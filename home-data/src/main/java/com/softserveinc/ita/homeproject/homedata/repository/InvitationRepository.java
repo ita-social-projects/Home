9ebe641a18445e50d8843cb92e2ad0470318218a
BenAvleck <ben.avleck@gmail.com>
2021-06-03 16:00:04 +0300

    Issue#182: CheckStyle changes.

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/repository/InvitationRepository.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/repository/InvitationRepository.java
@@ -4,8 +4,6 @@ import com.softserveinc.ita.homeproject.homedata.entity.Invitation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 
 @Repository
 public interface InvitationRepository extends JpaRepository<Invitation, Long> {
