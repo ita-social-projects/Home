e22777b668536f63e1d7c1f7ce9a0cf1e85e2aff
Rossokha Ilya <ayliross21@gmail.com>
2021-06-10 13:12:50 +0300

    Issue#228 permissions

--- home-application/src/main/java/com/softserveinc/ita/homeproject/application/api/PollApiImpl.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/application/api/PollApiImpl.java
@@ -1,5 +1,10 @@
 package com.softserveinc.ita.homeproject.application.api;
 
+import static com.softserveinc.ita.homeproject.application.constants.Permissions.CREATE_QUESTION_PERMISSION;
+import static com.softserveinc.ita.homeproject.application.constants.Permissions.DELETE_QUESTION_PERMISSION;
+import static com.softserveinc.ita.homeproject.application.constants.Permissions.GET_QUESTION_PERMISSION;
+import static com.softserveinc.ita.homeproject.application.constants.Permissions.UPDATE_QUESTION_PERMISSION;
+
 import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -14,7 +19,9 @@ import com.softserveinc.ita.homeproject.model.ReadMultipleChoiceQuestion;
 import com.softserveinc.ita.homeproject.model.UpdateQuestion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
+
 
 
 @Provider
@@ -24,6 +31,7 @@ public class PollApiImpl extends CommonApi implements PollsApi {
     @Autowired
     private PollQuestionService pollQuestionService;
 
+    @PreAuthorize(CREATE_QUESTION_PERMISSION)
     @Override
     public Response createQuestion(Long pollId, @Valid CreateQuestion createQuestion) {
         var createQuestionDto = mapper.convert(createQuestion, PollQuestionDto.class);
@@ -33,12 +41,14 @@ public class PollApiImpl extends CommonApi implements PollsApi {
         return Response.status(Response.Status.CREATED).entity(readQuestion).build();
     }
 
+    @PreAuthorize(DELETE_QUESTION_PERMISSION)
     @Override
     public Response deleteQuestion(Long pollId, Long id) {
         pollQuestionService.deactivatePollQuestion(pollId, id);
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 
+    @PreAuthorize(GET_QUESTION_PERMISSION)
     @Override
     public Response getQuestion(Long pollId, Long id) {
         PollQuestionDto toGet = pollQuestionService.getOne(id, getSpecification());
@@ -47,6 +57,7 @@ public class PollApiImpl extends CommonApi implements PollsApi {
         return Response.status(Response.Status.OK).entity(readQuestion).build();
     }
 
+    @PreAuthorize(GET_QUESTION_PERMISSION)
     @Override
     public Response queryQuestion(@Min(1L) Long pollId,
                                   @Min(1) Integer pageNumber,
@@ -56,6 +67,7 @@ public class PollApiImpl extends CommonApi implements PollsApi {
         return buildQueryResponse(readQuestion, ReadMultipleChoiceQuestion.class);
     }
 
+    @PreAuthorize(UPDATE_QUESTION_PERMISSION)
     @Override
     public Response updateQuestion(Long pollId, Long id, @Valid UpdateQuestion updateQuestion) {
         var updateQuestionDto = mapper.convert(updateQuestion, PollQuestionDto.class);
