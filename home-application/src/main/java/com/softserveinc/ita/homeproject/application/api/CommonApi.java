1de3325eea23b0fb22951d2807c26c7a428d54d6
Rossokha Ilya <ayliross21@gmail.com>
2021-06-30 16:29:33 +0300

    Issue#248 provided last version and dependencies

--- home-application/src/main/java/com/softserveinc/ita/homeproject/application/api/CommonApi.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/application/api/CommonApi.java
@@ -9,6 +9,7 @@ import javax.ws.rs.core.UriInfo;
 import com.softserveinc.ita.homeproject.application.mapper.HomeMapper;
 import com.softserveinc.ita.homeproject.application.service.QueryApiService;
 import com.softserveinc.ita.homeproject.homeservice.dto.BaseDto;
+import com.softserveinc.ita.homeproject.model.BaseReadView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
