9b9ec3a5cca77cdca8c0cb426c9472ea1b51a0dd
Illia-Shchur <i.shur322@gmail.com>
2021-07-12 15:28:47 +0300

    Issue#249-Added github secret. Code smells fixed

--- home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/quartz/config/QuartzUtil.java
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/quartz/config/QuartzUtil.java
@@ -8,25 +8,12 @@ import org.quartz.CronTrigger;
 import org.quartz.Job;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
-import org.quartz.SimpleTrigger;
 import org.quartz.TriggerBuilder;
 
 @Slf4j
 public class QuartzUtil {
 
-
-    public static SimpleTrigger createSimpleTrigger(JobDetail jobDetail, int quartzTimeJob, String triggerName) {
-        log.debug("createTrigger(jobDetail={}, pollFrequencyMs={}, triggerName={})",
-                jobDetail.toString(), quartzTimeJob, triggerName);
-
-        return TriggerBuilder.newTrigger()
-                .forJob(jobDetail)
-                .withIdentity(triggerName,"AutoConfigured")
-                .withSchedule(simpleSchedule()
-                        .repeatForever()
-                        .withIntervalInMilliseconds(quartzTimeJob)
-                        .withMisfireHandlingInstructionNextWithRemainingCount())
-                .build();
+    private QuartzUtil() {
     }
 
     public static CronTrigger createCronTrigger(JobDetail jobDetail, String cron, String triggerName, String group) {
@@ -39,7 +26,6 @@ public class QuartzUtil {
                 .withSchedule(cronSchedule(cron))
                 .build();
     }
-
 
     public static JobDetail createJobDetail(Class<? extends Job> jobClass, String jobName,String group) {
         log.debug("createJobDe tail(jobClass={}, jobName={})", jobClass.getName(), jobName);
