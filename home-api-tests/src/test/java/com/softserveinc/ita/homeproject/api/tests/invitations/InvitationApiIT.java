bc14a183d2e9bdc3c9691ea8d46feb66156f24ef
Illia-Shchur <i.shur322@gmail.com>
2021-07-14 14:23:21 +0300

    Issue#249-disabled isEmailSentTest.

--- home-api-tests/src/test/java/com/softserveinc/ita/homeproject/api/tests/invitations/InvitationApiIT.java
+++ home-api-tests/src/test/java/com/softserveinc/ita/homeproject/api/tests/invitations/InvitationApiIT.java
@@ -8,6 +8,7 @@ import com.softserveinc.ita.homeproject.api.tests.utils.mail.mock.ApiUsageFacade
 import com.softserveinc.ita.homeproject.api.tests.utils.mail.mock.dto.MailHogApiResponse;
 import com.softserveinc.ita.homeproject.model.*;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -22,6 +23,7 @@ class InvitationApiIT {
     private final CooperationApi cooperationApi = new CooperationApi(ApiClientUtil.getClient());
 
     @Test
+    @Disabled("Disabled for further refactor")
     void isEmailSentTest() throws Exception {
         CreateCooperation createCoop = createCooperation();
         ApiResponse<ReadCooperation> cooperationWithHttpInfo = cooperationApi.createCooperationWithHttpInfo(createCoop);
