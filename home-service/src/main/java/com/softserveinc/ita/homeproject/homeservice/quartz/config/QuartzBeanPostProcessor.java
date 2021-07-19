c3aa284d7895092c0ef281113eaa444a4b6f0f73
Illia-Shchur <i.shur322@gmail.com>
2021-07-19 15:07:49 +0300

    Issue#249-Removed extra line.

--- home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/quartz/config/QuartzBeanPostProcessor.java
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/quartz/config/QuartzBeanPostProcessor.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class QuartzBeanPostProcessor implements BeanFactoryPostProcessor {
 
