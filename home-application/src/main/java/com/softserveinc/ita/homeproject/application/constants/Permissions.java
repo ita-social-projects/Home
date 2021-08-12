36c1ea70429b36b4f2280b415a8b72c532a54fa9
Vladyslav-Frolov <tertey7@gmail.com>
2021-08-12 10:44:39 +0300

    Issue#272-Implement-Role-Matrix --> feat: Added: ChangeSet to DB Fixed: beautify Permissions

--- home-application/src/main/java/com/softserveinc/ita/homeproject/application/constants/Permissions.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/application/constants/Permissions.java
@@ -12,20 +12,23 @@ public final class Permissions {
 
     }
 
-    public static final String CREATE_DELETE_COOPERATION = "hasAuthority('CREATE_DELETE_COOPERATION')";
-
-    public static final String READ_COOPERATION = "hasAuthority('READ_COOPERATION')";
-    public static final String READ_COOPERATION_DATA = "hasAuthority('READ_COOPERATION_DATA')";
-    public static final String READ_APARTMENT_INFO = "hasAuthority('READ_APARTMENT_INFO')";
     public static final String READ_NEWS = "hasAuthority('READ_NEWS')";
-    public static final String READ_POLL = "hasAuthority('READ_POLL')";
-
-    public static final String UPDATE_COOPERATION = "hasAuthority('UPDATE_COOPERATION')";
+    public static final String MANAGE_NEWS = "hasAuthority('MANAGE_NEWS')";
 
     public static final String MANAGE_USER = "hasAuthority('MANAGE_USER')";
-    public static final String MANAGE_IN_COOPERATION = "hasAuthority('MANAGE_IN_COOPERATION')";
+
+    public static final String READ_COOPERATION = "hasAuthority('READ_COOPERATION')";
+    public static final String CREATE_DELETE_COOPERATION = "hasAuthority('CREATE_DELETE_COOPERATION')";
+    public static final String UPDATE_COOPERATION = "hasAuthority('UPDATE_COOPERATION')";
+
+    public static final String READ_COOPERATION_DATA = "hasAuthority('READ_COOPERATION_DATA')";
     public static final String MANAGE_COOPERATION_DATA = "hasAuthority('MANAGE_COOPERATION_DATA')";
-    public static final String MANAGE_NEWS = "hasAuthority('MANAGE_NEWS')";
+
+    public static final String MANAGE_IN_COOPERATION = "hasAuthority('MANAGE_IN_COOPERATION')";
+
+    public static final String READ_APARTMENT_INFO = "hasAuthority('READ_APARTMENT_INFO')";
+
+    public static final String READ_POLL = "hasAuthority('READ_POLL')";
     public static final String MANAGE_POLLS = "hasAuthority('MANAGE_POLLS')";
 
 }
