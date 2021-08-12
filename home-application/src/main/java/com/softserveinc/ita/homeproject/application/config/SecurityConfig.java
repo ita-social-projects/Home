dcdd1715c6272cd51fb2756880e393d7476507b2
Vladyslav-Frolov <tertey7@gmail.com>
2021-08-12 12:10:22 +0300

    Issue#272-Implement-Role-Matrix --> feat: Fixed: SecurityConfig permit to apidocs Fixed: ChangeSet

--- home-application/src/main/java/com/softserveinc/ita/homeproject/application/config/SecurityConfig.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/application/config/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "*/users").permitAll()
+                .antMatchers(HttpMethod.GET, "*/api/*/apidocs/*").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
