b6204bc02daa7fdff4352fe92015b7243f5fc7dc
Stas Kolesnychenko <olimpixks@gmail.com>
2021-10-18 18:57:50 +0300

    Issue#313 Fixed the problem with adding appartment to the house. Fixed the bug when we add several apartments with the same number.

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/cooperation/apatment/ApartmentRepository.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/cooperation/apatment/ApartmentRepository.java
@@ -4,8 +4,12 @@ import com.softserveinc.ita.homeproject.homedata.cooperation.apatment.Apartment;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.Optional;
+
 
 public interface ApartmentRepository extends PagingAndSortingRepository<Apartment, Long>,
-        JpaSpecificationExecutor<Apartment> {
+    JpaSpecificationExecutor<Apartment> {
     Apartment findApartmentByApartmentNumber(String apartmentNumber);
+
+    Optional<Apartment> findByApartmentNumberAndHouseId(String apartmentNumber, Long houseId);
 }
