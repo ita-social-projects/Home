106471cee129d736f434780febaf0779d45a6088
vladyslavyarets <zzzlpzzz0@gmail.com>
2022-04-16 09:42:27 +0300

    Change Vote entity due to new DB structure

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/poll/votes/Vote.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/poll/votes/Vote.java
@@ -1,16 +1,19 @@
 package com.softserveinc.ita.homeproject.homedata.poll.votes;
 
-import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import com.softserveinc.ita.homeproject.homedata.BaseEntity;
+import com.softserveinc.ita.homeproject.homedata.poll.Poll;
+import com.softserveinc.ita.homeproject.homedata.poll.converters.PollQuestionTypeAttributeConverter;
+import com.softserveinc.ita.homeproject.homedata.poll.enums.PollQuestionType;
+import com.softserveinc.ita.homeproject.homedata.poll.question.PollQuestion;
 import com.softserveinc.ita.homeproject.homedata.user.User;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,13 +25,26 @@ import lombok.Setter;
 @SequenceGenerator(name = "sequence", sequenceName = "votes_sequence")
 public class Vote extends BaseEntity {
 
-    @Column(name = "poll_id")
-    private Long pollId;
+    @ManyToOne
+    @JoinColumn(name = "poll_id", nullable = false)
+    private Poll poll;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @OneToMany(mappedBy = "vote", cascade = CascadeType.PERSIST)
-    private List<QuestionVote> questionVotes;
+    @Convert(converter = PollQuestionTypeAttributeConverter.class)
+    @Column(name = "type", insertable = false, updatable = false)
+    private PollQuestionType type;
+
+    @Column(name = "advice_answer")
+    private String adviceAnswer;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private PollQuestion question;
+
+    @OneToOne(mappedBy = "vote")
+    private VoteAnswerVariant voteAnswerVariant;
+
 }
