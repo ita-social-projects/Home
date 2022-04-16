38517ca40dc1afd5017335f3c64067c5227fad2b
vladyslavyarets <zzzlpzzz0@gmail.com>
2022-04-16 09:14:40 +0300

    Revert misleading changes of VoteDto

--- home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/dto/poll/votes/VoteDto.java
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/dto/poll/votes/VoteDto.java
@@ -1,21 +1,17 @@
 package com.softserveinc.ita.homeproject.homeservice.dto.poll.votes;
 
-import com.softserveinc.ita.homeproject.homedata.poll.Poll;
+import java.util.List;
+
 import com.softserveinc.ita.homeproject.homeservice.dto.BaseDto;
-import com.softserveinc.ita.homeproject.homeservice.dto.poll.question.PollQuestionDto;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class VoteDto extends BaseDto {
-    private Poll poll;
+    private Long pollId;
 
     private Long userId;
 
-    private String type;
-
-    private String adviceAnswer;
-
-    private PollQuestionDto questionDto;
+    private List<QuestionVoteDto> questionVotes;
 }
