93a90234c731c2f9014a911cc7787907df55ec6d
vladyslavyarets <zzzlpzzz0@gmail.com>
2022-04-16 09:06:49 +0300

    Change Issue412.xml: Add sequence

--- home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/dto/poll/votes/VoteDto.java
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/dto/poll/votes/VoteDto.java
@@ -1,17 +1,21 @@
 package com.softserveinc.ita.homeproject.homeservice.dto.poll.votes;
 
-import java.util.List;
-
+import com.softserveinc.ita.homeproject.homedata.poll.Poll;
 import com.softserveinc.ita.homeproject.homeservice.dto.BaseDto;
+import com.softserveinc.ita.homeproject.homeservice.dto.poll.question.PollQuestionDto;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class VoteDto extends BaseDto {
-    private Long pollId;
+    private Poll poll;
 
     private Long userId;
 
-    private List<QuestionVoteDto> questionVotes;
+    private String type;
+
+    private String adviceAnswer;
+
+    private PollQuestionDto questionDto;
 }
