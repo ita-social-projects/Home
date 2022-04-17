50932739da9ebd22c910db4554866b24d012e766
vladyslavyarets <zzzlpzzz0@gmail.com>
2022-04-17 18:51:54 +0300

    Change ResultQuestion entity due to last table modification

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/poll/results/ResultQuestion.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/poll/results/ResultQuestion.java
@@ -1,40 +1,32 @@
 package com.softserveinc.ita.homeproject.homedata.poll.results;
 
 import javax.persistence.Column;
-import javax.persistence.Convert;
-import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import com.softserveinc.ita.homeproject.homedata.BaseEntity;
-import com.softserveinc.ita.homeproject.homedata.poll.converters.PollQuestionTypeAttributeConverter;
-import com.softserveinc.ita.homeproject.homedata.poll.enums.PollQuestionType;
-import com.softserveinc.ita.homeproject.homedata.poll.question.PollQuestion;
+import com.softserveinc.ita.homeproject.homedata.poll.question.AnswerVariant;
 import lombok.Getter;
 import lombok.Setter;
+
 
 @Getter
 @Setter
 @Entity
 @Table(name = "result_questions")
 @SequenceGenerator(name = "sequence", sequenceName = "result_questions_sequence")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "type")
-public abstract class ResultQuestion extends BaseEntity {
-
-    @Convert(converter = PollQuestionTypeAttributeConverter.class)
-    @Column(name = "type", insertable = false, updatable = false)
-    private PollQuestionType type;
+public class ResultQuestion extends BaseEntity {
 
     @OneToOne
-    @JoinColumn(name = "question_id")
-    private PollQuestion question;
+    @JoinColumn(name = "answer_variant_id")
+    private AnswerVariant answerVariant;
 
     @Column(name = "vote_count")
     private int voteCount;
+
+    @Column(name = "percent_votes")
+    private String percentVotes;
 }
