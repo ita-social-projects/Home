b20fa83e8b5a7b176d9a92eb2f99f9dda7730653
Vadim Kozhevnikov <77holder@gmail.com>
2022-04-18 09:40:54 +0300

    @Builder for dtos from folder: src/main/java/com/softserveinc/ita/homeproject/homeservice/dto/general/contact

--- home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/dto/general/contact/EmailContactDto.java
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/dto/general/contact/EmailContactDto.java
@@ -1,10 +1,12 @@
 package com.softserveinc.ita.homeproject.homeservice.dto.general.contact;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class EmailContactDto extends ContactDto {
 
     private String email;
--- home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/dto/general/contact/PhoneContactDto.java
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/dto/general/contact/PhoneContactDto.java
@@ -1,10 +1,12 @@
 package com.softserveinc.ita.homeproject.homeservice.dto.general.contact;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class PhoneContactDto extends ContactDto {
 
     private String phone;
