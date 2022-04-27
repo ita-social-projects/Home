1bf56e864d6906709d6fbe81d7fd61dcf3b1acb8
Artem <puhachofficial@gmail.com>
2022-04-27 14:19:56 +0300

    Created unit tests for JWTProvider and added some configuration files for it.

---
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/application/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.softserveinc.ita.homeproject.application.config;
+
+import com.softserveinc.ita.homeproject.application.security.filter.JWTProvider;
+import com.softserveinc.ita.homeproject.application.security.filter.JWTTokenValidatorFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+/**
+ * SecurityConfig class configures security in application.
+ * Class provides HttpBasic auth for the application.
+ *
+ * @author Ihor Svyrydenko
+ */
+@Configuration
+@EnableGlobalMethodSecurity(
+    prePostEnabled = true,
+    securedEnabled = true,
+    jsr250Enabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JWTProvider jwtProvider;
+
+    @Autowired
+    public SecurityConfig(@Qualifier("homeUserDetailsService") UserDetailsService userDetailsService,
+                          JWTProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .csrf().disable()
+            .cors().configurationSource(request ->
+                getCorsConfiguration())
+            .and()
+            .addFilterBefore(new JWTTokenValidatorFilter(jwtProvider),
+                BasicAuthenticationFilter.class);
+    }
+
+    private CorsConfiguration getCorsConfiguration() {
+        var corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+        return corsConfiguration;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
