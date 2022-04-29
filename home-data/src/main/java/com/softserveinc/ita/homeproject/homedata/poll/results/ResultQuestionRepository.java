ee40d6e33a7bed5c51f606c8aa3764783bc6060b
vladyslavyarets <zzzlpzzz0@gmail.com>
2022-04-29 13:33:40 +0300

    Add findAllByPoll in ResultQuestionRepository

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/poll/results/ResultQuestionRepository.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/poll/results/ResultQuestionRepository.java
@@ -1,12 +1,17 @@
 package com.softserveinc.ita.homeproject.homedata.poll.results;
 
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
+import com.softserveinc.ita.homeproject.homedata.poll.Poll;
 import com.softserveinc.ita.homeproject.homedata.poll.question.AnswerVariant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ResultQuestionRepository extends JpaRepository<ResultQuestion, Long> {
+    Collection<ResultQuestion> findAllByPoll(Poll poll);
 }
