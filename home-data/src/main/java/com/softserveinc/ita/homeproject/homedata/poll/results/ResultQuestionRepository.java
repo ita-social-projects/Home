e9103ed4dc74c302d87da949e12273fd65bc1f2b
vladyslavyarets <zzzlpzzz0@gmail.com>
2022-05-11 11:23:22 +0300

    Remove bad imports

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/poll/results/ResultQuestionRepository.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/poll/results/ResultQuestionRepository.java
@@ -1,13 +1,9 @@
 package com.softserveinc.ita.homeproject.homedata.poll.results;
 
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
 
 import com.softserveinc.ita.homeproject.homedata.poll.Poll;
-import com.softserveinc.ita.homeproject.homedata.poll.question.AnswerVariant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/poll/votes/VoteRepository.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/poll/votes/VoteRepository.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import com.softserveinc.ita.homeproject.homedata.poll.Poll;
 import com.softserveinc.ita.homeproject.homedata.user.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
