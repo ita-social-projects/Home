62268324adcc3f636d3f7352612f3f9559bde04b
Artem <puhachofficial@gmail.com>
2022-05-13 20:48:26 +0300

    Changed mappers for NotAcceptableOauthException

--- home-application/src/main/java/com/softserveinc/ita/homeproject/application/exception/mapper/BaseOauthExceptionMapper.java
+++ home-application/src/main/java/com/softserveinc/ita/homeproject/application/exception/mapper/BaseOauthExceptionMapper.java
@@ -1,15 +1,8 @@
 package com.softserveinc.ita.homeproject.application.exception.mapper;
 
-import javax.ws.rs.core.Response;
-
 import com.softserveinc.ita.homeproject.application.security.exception.BaseOauthException;
 
-public class BaseOauthExceptionMapper<T extends BaseOauthException> extends BaseExceptionMapper<T> {
-
-    @Override
-    protected Response.Status getStatus() {
-        return Response.Status.NOT_ACCEPTABLE;
-    }
+public abstract class BaseOauthExceptionMapper<T extends BaseOauthException> extends BaseExceptionMapper<T> {
 
     @Override
     protected String extractMessage(T exception) {
