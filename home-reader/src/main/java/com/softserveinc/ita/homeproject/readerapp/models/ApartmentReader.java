3aff379b5e58579be44b628c272a2077ba27b544
vzhyh <zhyhyr@progress.com>
2022-05-20 15:18:40 +0300

    added new model

--- home-reader/src/main/java/com/softserveinc/ita/homeproject/readerapp/models/ApartmentReader.java
+++ home-reader/src/main/java/com/softserveinc/ita/homeproject/readerapp/models/ApartmentReader.java
@@ -1,26 +1,29 @@
 package com.softserveinc.ita.homeproject.readerapp.models;
 
 
+import javax.persistence.SequenceGenerator;
+
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import javax.persistence.SequenceGenerator;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Document
 @SequenceGenerator(name = "sequence", sequenceName = "apartments_sequence")
 public class ApartmentReader {
+
+    @Id
     private String apartmentNumber;
 
-    private BigDecimal apartmentArea;
-
-    private LocalDateTime createDate;
-
-    private LocalDateTime updateDate;
-
     private Boolean enabled;
+
+    public ApartmentReader(String apartmentNumber, Boolean enabled) {
+        this.apartmentNumber = apartmentNumber;
+        this.enabled = enabled;
+    }
+
+    public ApartmentReader() {
+    }
 }
