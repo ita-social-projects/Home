899300bc7256790ef87eb5e7a64348e0243b853f
Artem <puhachofficial@gmail.com>
2022-06-22 14:17:43 +0300

    Added CrossOrigin Annotation to prevent rejected anwers from the OauthServer

--- home-oauth-server/src/main/java/com/softserveinc/ita/homeproject/homeoauthserver/controller/OauthController.java
+++ home-oauth-server/src/main/java/com/softserveinc/ita/homeproject/homeoauthserver/controller/OauthController.java
@@ -14,9 +14,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@CrossOrigin
 @RestController
 @RequestMapping("/api/0/oauth2")
 public class OauthController implements ServerApi {
