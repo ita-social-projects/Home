81cfddbe3403441e2a03ddc115fe724f7625cf87
vladyslavyarets <zzzlpzzz0@gmail.com>
2022-07-23 01:42:43 +0300

    Add PasswordRestorationException and its mapper

---
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/exception/PasswordRestorationException.java
@@ -0,0 +1,9 @@
+package com.softserveinc.ita.homeproject.homeservice.exception;
+
+
+public class PasswordRestorationException extends BaseHomeException{
+
+    public PasswordRestorationException(String message) {
+        super(message);
+    }
+}
