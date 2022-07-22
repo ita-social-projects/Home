fb27b9224927cb2d52bd02d589d1a03a4a9f6e75
vladyslavyarets <zzzlpzzz0@gmail.com>
2022-07-22 14:06:26 +0300

    Change PasswordRecovery
    Add field status
    Change expiration_datetime to sent_datetime

--- home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/user/password/PasswordRecovery.java
+++ home-data/src/main/java/com/softserveinc/ita/homeproject/homedata/user/password/PasswordRecovery.java
@@ -2,20 +2,24 @@ package com.softserveinc.ita.homeproject.homedata.user.password;
 
 import java.time.LocalDateTime;
 import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import com.softserveinc.ita.homeproject.homedata.BaseEntity;
+import com.softserveinc.ita.homeproject.homedata.cooperation.invitation.converters.PasswordRecoveryTokenStatusAttributeConverter;
+import com.softserveinc.ita.homeproject.homedata.user.password.enums.PasswordRecoveryTokenStatus;
 import lombok.Getter;
 import lombok.Setter;
+
 
 @Getter
 @Setter
 @Entity
 @Table(name = "password_recovery_tokens")
 @SequenceGenerator(name = "sequence", sequenceName = "password_recovery_tokens_sequence")
-public class PasswordRecoveryToken extends BaseEntity {
+public class PasswordRecovery extends BaseEntity {
 
     @Column(name = "email")
     private String email;
@@ -25,6 +29,10 @@ public class PasswordRecoveryToken extends BaseEntity {
 
     @Column(name = "sent_datetime")
     private LocalDateTime sentDateTime;
+
+    @Convert(converter = PasswordRecoveryTokenStatusAttributeConverter.class)
+    @Column(name = "status")
+    private PasswordRecoveryTokenStatus status;
 
     @Column(name = "enabled")
     private Boolean enabled;
