8ad0e0f22b8dab274fe040fd66eca30e28bae516
vladyslavyarets <zzzlpzzz0@gmail.com>
2022-07-28 16:10:57 +0300

    Remove bad imports

--- home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/dto/cooperation/invitation/InvitationDto.java
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/dto/cooperation/invitation/InvitationDto.java
@@ -1,7 +1,5 @@
 package com.softserveinc.ita.homeproject.homeservice.dto.cooperation.invitation;
 
-import com.softserveinc.ita.homeproject.homedata.general.address.Address;
-import com.softserveinc.ita.homeproject.homeservice.dto.BaseDto;
 import com.softserveinc.ita.homeproject.homeservice.dto.cooperation.invitation.enums.InvitationStatusDto;
 import com.softserveinc.ita.homeproject.homeservice.dto.cooperation.invitation.enums.InvitationTypeDto;
 import com.softserveinc.ita.homeproject.homeservice.service.general.email.Mailable;
